1236bb056bca4a13dbde6c327397be1b2a17c367
Rajkrishna Debnath <rajkrishnadebnath53@gmail.com>
2024-03-23 17:07:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.btn'); 
+
+let snakeArr = [
+    4, 3, 6
+]
+
+btn.addEventListener('click', () => {
+
+    snakeArr.forEach((e, i) => {
+    console.log(e);
+    
+})
+})
