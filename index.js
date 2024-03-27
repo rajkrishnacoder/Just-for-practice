ebd0fc237bfca01b51004360d07d2c737067a716
Rajkrishna Debnath <rajkrishnadebnath53@gmail.com>
2024-03-27 19:31:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('.btn'); 
-
-let snakeArr = [
-    4, 3, 6
-]
-
+let test = ["hello",
+   {name: 'rajkrishna', 
+    age: 20
+   }
+];//so this way i can do so many things
+const dip = "Dip Debnath";
+let hello = 0;
+console.log(dip);
 btn.addEventListener('click', () => {
-
-    snakeArr.forEach((e, i) => {
-     console.log(e);
-    
-    })
-})
+   hello++
+   btn.innerText = hello;
+});
