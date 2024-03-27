81349252c04ed35d5d50f855c3bb5299265a01d0
Rajkrishna Debnath <rajkrishnadebnath53@gmail.com>
2024-03-27 20:16:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const btn = document.querySelector('.btn'); 
-let test = ["hello",
-   {name: 'rajkrishna', 
-    age: 20
-   }
-];//so this way i can do so many things
-const dip = "Dip Debnath";
-let hello = 0;
-console.log(dip);
-btn.addEventListener('click', () => {
-   hello++
-   btn.innerText = hello;
-});
+const val = (what) => {
+   return what ? 'what' : 'bad';
+}
+
+val('bad');
