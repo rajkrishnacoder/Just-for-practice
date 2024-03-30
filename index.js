d12ee258a63079929f2fb51c3cfd35687d51cb14
Rajkrishna Debnath <rajkrishnadebnath53@gmail.com>
2024-03-30 19:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const points = [40, 100, 1, 5, 25, 10];
-points.sort(function(a, b){return a - b});
 
-console.log(points);
+const time = () => {
+ const d = new Date().getSeconds();
+ console.log(d);   
+}
+
+
+
+setInterval(time, 1000);
