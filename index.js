2389c124685827aeecd102f048fc5a992491f5bb
Rajkrishna Debnath <rajkrishnadebnath53@gmail.com>
2024-03-31 17:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// const cars = [
+//   {type:"Volvo", year:2016},
+//   {type:"Saab", year:2001},
+//   {type:"BMW", year:2010}
+// ];
 
-const time = () => {
- const d = new Date().getSeconds();
- console.log(d);   
-}
+// cars.sort((a, b) => {
+//     let x = a.type.toLowerCase();
+//     let y = b.type.toLowerCase();
+//     if (x < y) {return -1}
+//     if (x > y) {return 1}
+// });
+// console.log(cars);
+// let text = "The rain in SPAIN stays mainly in the plain";
 
+// console.log(text.match(/ain/));
+const numbers = [45, 4, 9, 16, 25];
 
-
-setInterval(time, 1000);
+let num = numbers.flatMap((x) =>  x * 2 )
+console.log(num);
