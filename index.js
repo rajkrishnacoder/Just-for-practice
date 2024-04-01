a9e14c6a4411fd37acab3dde3940ab68e13f1ab1
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-01 19:02:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-// const cars = [
-//   {type:"Volvo", year:2016},
-//   {type:"Saab", year:2001},
-//   {type:"BMW", year:2010}
-// ];
 
-// cars.sort((a, b) => {
-//     let x = a.type.toLowerCase();
-//     let y = b.type.toLowerCase();
-//     if (x < y) {return -1}
-//     if (x > y) {return 1}
-// });
-// console.log(cars);
-// let text = "The rain in SPAIN stays mainly in the plain";
-
-// console.log(text.match(/ain/));
-// const myArr = [1, 2, 3, 4, 5, 6];
-// const newArr = myArr.flatMap((x, z, s) => x);
-// console.log(newArr);
