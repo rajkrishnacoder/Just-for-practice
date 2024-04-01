09f15b187dcb6ab6499e52ac3f1d7e9d483cd389
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-01 19:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('rajkrishna debanath');
+// I am writing this code on linux oparating system so this is new exeperiance for me
+// i have been using windows for quite long time so this kinda adventures
+console.log('rajkrishna debanath'); 
