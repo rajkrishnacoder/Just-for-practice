be993125c0c3d600728aef971ee2201a6e47048c
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-04 20:28:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const cars = ["BMW", "Volvo", "Saab", "Ford"];
-let i = 2;
-let len = cars.length;
-let text = "";
-for (; i < len; i++) {
-  text += cars[i] + "<br>";
-  console.log(i)
-}
-console.log(text);
-console.log(i);
+
