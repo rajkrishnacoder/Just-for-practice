6569c43c3edda9b1bb89b015ef568193d3805b64
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-04 17:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const keys = fruits.entries();
-let text = "";
-for (let x of keys) {
-  text += `${x} \n`
-}
-
-console.log(text);
+const test = Math.sign();
+console.log(test);
