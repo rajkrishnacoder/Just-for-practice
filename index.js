4b6077b0f6acace8bc121703ba76cc68f94f9c11
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-06 15:39:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index) {
+  txt += value;
+}
+console.log(txt)
