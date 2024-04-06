10497ca90ad50b04291898c5e799ecc2a90b25e9
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-06 20:41:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const numbers = [45, 4, 9, 16, 25];
-
-let txt = "";
-numbers.forEach(myFunction);
-
-function myFunction(value, index) {
-  txt += value;
-}
-console.log(txt)
+const letters = new Set(["a","b","c"]);
+letters.add("a");
+letters.add("b");
+letters.add("c");
+letters.add("c");
+letters.add("c");
+letters.add("c");
+letters.add("c");
+letters.add("c");
+console.log(letters)
