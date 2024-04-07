057068434a81a6ad5a820e94a323f3c903f39e10
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-07 08:10:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const fruits = new Map();
-
-fruits.set('apples', 400);
-fruits.set('bananas', 300);
-fruits.set('oranges', 200);
-fruits.set('mangos', 300)
-console.log(fruits.delete('mangos'))
-console.log(fruits)
+console.log(typeof function name(){})
