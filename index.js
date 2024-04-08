efe1d78bb6e70392717de0139a722d8d6a752d6f
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-08 11:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function myName(name1, name2) {
-   this.firstName = name1;
-   this.lastName = name2;
+const person = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
 }
-const ke = new myName('rajkrishna', 'Debnath');
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+const person2 = {
+  firstName: "Mary",
+  lastName: "Doe"
+}
 
-console.log(ke);
+// This will return "Mary Doe":
+console.log(person.fullName.apply(person1));
