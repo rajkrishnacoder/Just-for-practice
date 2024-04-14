bf7fe07cc68cdbbae73daf355814389d8f6012f0
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-14 19:13:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log('i need to do my best for right now ')
+console.log('i need to do my best for right now ');
+console.log('this is reality i have to work hard for success it is impotent')
