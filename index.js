a4df971546c1cfaefe62d9bee3a0e92f2285355d
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-17 19:47:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const myFunction = (...number) => number
-console.log(myFunction(22, 30, 40, 50))
+
+
+const myObject = {
+    name: "rajkrishna", 
+    age: 20,
+    village: "gokulpur",
+    myFunc2: function() {
+        console.log(`your age is ${this.age}`)
+    }
+}
+
+myObject.myFunc = function() {
+    console.log(`hello, ${this.name}`);
+}
+console.log(myObject.myFunc());
+console.log(myObject.myFunc2())
