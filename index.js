674c68b82cdbef156cd966c2907bf3e4d92d932f
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-17 19:07:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-const myArray = [1, 2, 3, 4, 5]
-const marval_heroes = ["thor", "Ironman", "spiderman"]
-const newArray = [...myArray, ...marval_heroes]
-console.log(newArray)
+ (() => console.log('hello'))();
+ (() => console.log('hello world'))()
