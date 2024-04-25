2bb3b9fd2035cb574c41cea1f98964001057d283
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-04-25 20:05:33 +0530

    Update practice.js

--- experimental/practice.js
+++ experimental/practice.js
@@ -1,16 +1,41 @@
-function user(username) {
-    this._username = username
+class user{
+    constructor(username, email, age) {
+        this.username = username
+        this.email = email
+        this.age = age
+    }
 
-    Object.defineProperty(this, 'username', {
-        get: function() {
-            return this._username.toUpperCase()
-        },
-        set: function(value) {
-            this._username = value
-        }
-    })
+    get username() {
+        return this._username.toUpperCase()
+    }
+    set username(value) {
+        this._username = value
+    }
+    test() {
+        console.log("hellow world")
+    }
 }
 
-const user1 = new user("rajkrishna") 
+class myUser extends user{
+    constructor(username, email, age, id) {
+        super(username, email, age) 
+        this.id = id
+    }
+    get id() {
+        return typeof String(this._id)
+    }
+    set id(value) {
+        this._id = value
+    }
 
-console.log(user1.username) 
+    get age() {
+        return typeof Number(this._age)
+    }
+    set age(value) {
+        this._age = value
+    }
+}
+
+const user1 = new myUser("rajkrishna", "google.com" , "30", 123) 
+
+user1.test()
