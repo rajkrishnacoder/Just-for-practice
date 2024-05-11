eeaf0c75c39b2df4b5ab53c89b4d5bf1c8e87f72
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-05-11 07:33:19 +0530

    update

--- experimental/practice.js
+++ experimental/practice.js
@@ -1,127 +1,42 @@
-class Node{
-    constructor(val, next){
-        this.val = val
-        this.next = next
-    }
-}
-
-// this is a helper class for ower singly linked lists
-
-class SinglyLinkedLists{
+class MaxBinaryHeap{
     constructor(){
-        this.head = null
-        this.tail = null
-        this.length = 0;
+        this.value = []
     }
-
-    push(val){
-        let newNode = new Node(val, null)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        }else{
-            this.tail.next = newNode
-            this.tail = this.tail.next
+    Insert(element){
+        this.value.push(element)
+        this.bubbleUP()
+    }
+    bubbleUP(){
+        let inx = this.value.length - 1
+        const element = this.value[inx]
+        while(inx > 0){
+            let parentInx = Math.floor((inx-1)/2)
+            let parent = this.value[parentInx]
+            if(element <= parent || !element) break
+            this.value[parentInx] = element
+            this.value[inx] = parent
+            inx = parentInx
         }
-        this.length++
-        return this
     }
-    pop(){
-        let current = this.head
-        if(this.length === 1){
-            this.head = null
-            this.tail = null
-        }else{
-            let prev = current
-            for(let i = 0; i < this.length-1; i++){
-                prev = current
-                current = current.next
-            } 
-            this.tail = prev
-            this.tail.next = null 
-        }
-        this.length--
-        return current
+    extractMax(){
+        const max = this.value[0]
+        const end = this.value.pop()
+        this.value[0] = end
+        this.sinkDown()
+        return max
     }
-    shift(){
-       if(!this.head) return undefined
-       let currentHead = this.head
-       this.head = currentHead.next
-       if(this.length === 1){
-        this.tail = null
-       }
-       this.length--
-       return currentHead
-    }
-    unshift(val){
-        let newNode = new Node(val, null)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        }else{
-            newNode.next = this.head
-            this.head = newNode
-        }
-        this.length++
-        return this
-    }
-    get(index){
-        if(index < 0 || index >= this.length) return false
-        let current = this.head
-        for(let i = 0; i < index; i++){
-            current = current.next
-        }
-        return current
-    }
-    set(index, val){
-        if(index < 0 || index >= this.length) return false
-        let changingIndex = this.get(index)
-        changingIndex.val = val
-        return true
-    }
-    insert(index, val){
-        if(index < 0 || index > this.length) return false
-        if(index === 0) return !!this.unshift(val)
-        if(index === this.length) return !!this.push(val)
-        let newNode = new Node(val, null)
-        let prev = this.get(index-1)
-        let cur = prev.next
-        prev.next = newNode
-        newNode.next = cur
-        this.length++
-        return true
-    }
-    remove(index){
-        if(index < 0 || index >= this.length) return false
-        if(index === 0) return !!this.shift()
-        if(index === this.length-1) return !!this.pop()
-        let prev = this.get(index-1)
-        let cur = prev.next
-        prev.next = cur.next
-        this.length--
-        return cur
-    }
-    reverse(){
-        let node = this.head
-        this.head = this.tail
-        this.tail = node 
-
-        let next;
-        let prev = null
-        for (let i = 0; i < this.length; i++){
-            next = node 
-            node = node.next
-            next.next = prev
-            prev = next
-        }
-        return this
+    sinkDown(){
+        let inx = 0;
+        const length = this.value.length
+        const element = this.value[0]
+        
     }
 }
 
-let list = new SinglyLinkedLists()
+let heap = new MaxBinaryHeap()
+heap.Insert(55)
+heap.Insert(1)
+heap.Insert(60)
+heap.Insert(79)
 
-list.push(1);
-list.push(2);
-
-list.reverse()
-console.log(list)
+console.log(heap)
