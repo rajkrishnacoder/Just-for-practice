a2418062fba4be5ebeca4971fee5e86acb1752b2
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-05-12 10:55:23 +0530

    Update practice.js

--- experimental/practice.js
+++ experimental/practice.js
@@ -1,9 +1,17 @@
+class Node{
+    constructor(val, priority){
+        this.val = val
+        this.priority = priority
+    }
+}
+
 class MaxBinaryHeap{
     constructor(){
         this.value = []
     }
-    Insert(element){
-        this.value.push(element)
+    Insert(element, priority){
+        let newNode = new Node(element, priority)
+        this.value.push(newNode)
         this.bubbleUp()
     }
     bubbleUp(){
@@ -12,20 +20,20 @@ class MaxBinaryHeap{
         while(inx > 0){
             let parentInx = Math.floor((inx-1)/2)
             let parent = this.value[parentInx]
-            if(element >= parent) break 
+            if(element.priority >= parent.priority) break 
             this.value[parentInx] = this.value[inx]
             this.value[inx] = parent
             inx = parentInx
         }
     }
     extract(){
-        let max = this.value[0]
+        let min = this.value[0]
         let end = this.value.pop()
         if(this.value.length > 0){
             this.value[0] = end
             this.sinking()
         }
-        return max
+        return min
     }
     sinking(){
         let inx = 0
@@ -38,13 +46,13 @@ class MaxBinaryHeap{
             let swap = null
             if(leftChildInx < length){
                 leftChild = this.value[leftChildInx]
-                if(leftChild < element){
+                if(leftChild.priority < element.priority){
                     swap = leftChildInx
                 }
             }
             if(rightChildInx < length){
                 rightChild = this.value[rightChildInx]
-                if(rightChild < element && rightChild < leftChild){
+                if(rightChild.priority < element.priority && rightChild.priority < leftChild.priority){
                     swap = rightChildInx
                 }
             }
@@ -57,13 +65,12 @@ class MaxBinaryHeap{
 }
 
 let heap = new MaxBinaryHeap()
-heap.Insert(55)
-heap.Insert(1)
-heap.Insert(60)
-heap.Insert(79)
 
-heap.extract()
-heap.extract()
+heap.Insert("head ach", 3)
+heap.Insert("urgent", 1)
+heap.Insert("feaver", 2)
+heap.Insert("slite pain", 5)
+
 heap.extract()
 
 console.log(heap)
