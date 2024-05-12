b99c3b65fed284af1d7dd0c4904adbd128d72652
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-05-12 10:40:03 +0530

    Update practice.js

--- experimental/practice.js
+++ experimental/practice.js
@@ -4,32 +4,55 @@ class MaxBinaryHeap{
     }
     Insert(element){
         this.value.push(element)
-        this.bubbleUP()
+        this.bubbleUp()
     }
-    bubbleUP(){
+    bubbleUp(){
         let inx = this.value.length - 1
-        const element = this.value[inx]
+        let element = this.value[inx]
         while(inx > 0){
             let parentInx = Math.floor((inx-1)/2)
             let parent = this.value[parentInx]
-            if(element <= parent || !element) break
-            this.value[parentInx] = element
+            if(element >= parent) break 
+            this.value[parentInx] = this.value[inx]
             this.value[inx] = parent
             inx = parentInx
         }
     }
-    extractMax(){
-        const max = this.value[0]
-        const end = this.value.pop()
-        this.value[0] = end
-        this.sinkDown()
+    extract(){
+        let max = this.value[0]
+        let end = this.value.pop()
+        if(this.value.length > 0){
+            this.value[0] = end
+            this.sinking()
+        }
         return max
     }
-    sinkDown(){
-        let inx = 0;
-        const length = this.value.length
-        const element = this.value[0]
-        
+    sinking(){
+        let inx = 0
+        let element = this.value[0]
+        let length = this.value.length
+        while(true){
+            let leftChildInx = inx * 2 + 1
+            let rightChildInx = inx * 2 + 2
+            let leftChild, rightChild
+            let swap = null
+            if(leftChildInx < length){
+                leftChild = this.value[leftChildInx]
+                if(leftChild < element){
+                    swap = leftChildInx
+                }
+            }
+            if(rightChildInx < length){
+                rightChild = this.value[rightChildInx]
+                if(rightChild < element && rightChild < leftChild){
+                    swap = rightChildInx
+                }
+            }
+            if(swap === null) break
+            this.value[inx] = this.value[swap]
+            this.value[swap] = element
+            inx = swap
+        }
     }
 }
 
@@ -38,5 +61,9 @@ heap.Insert(55)
 heap.Insert(1)
 heap.Insert(60)
 heap.Insert(79)
+
+heap.extract()
+heap.extract()
+heap.extract()
 
 console.log(heap)
