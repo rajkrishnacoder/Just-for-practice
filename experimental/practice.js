d013d034e2403e4dcfeafb895360295bea79c455
Rajkrishna Debnath <154726311+rajkrishnacoder@users.noreply.github.com>
2024-05-13 17:34:34 +0530

    Update practice.js

--- experimental/practice.js
+++ experimental/practice.js
@@ -1,76 +1,31 @@
-class Node{
-    constructor(val, priority){
-        this.val = val
-        this.priority = priority
+class HashTables{
+    constructor(size=17){
+        this.keyMap = new Array(size)
+    }
+    _Hash(value){
+        let total = 0
+        let primeNumber = 31
+        for(let i = 0; i < Math.min(value.length, 100); i++){
+            let char = value.charCodeAt(i) - 96
+            total = (total * primeNumber + char) % this.keyMap.length
+        }
+        return total
+    }
+    set(key, value){
+        let index = this._Hash(key)
+        console.log(index)
+        if(!this.keyMap[index]){
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value])
+    }
+    get(key){
+        
     }
 }
 
-class MaxBinaryHeap{
-    constructor(){
-        this.value = []
-    }
-    Insert(element, priority){
-        let newNode = new Node(element, priority)
-        this.value.push(newNode)
-        this.bubbleUp()
-    }
-    bubbleUp(){
-        let inx = this.value.length - 1
-        let element = this.value[inx]
-        while(inx > 0){
-            let parentInx = Math.floor((inx-1)/2)
-            let parent = this.value[parentInx]
-            if(element.priority >= parent.priority) break 
-            this.value[parentInx] = this.value[inx]
-            this.value[inx] = parent
-            inx = parentInx
-        }
-    }
-    extract(){
-        let min = this.value[0]
-        let end = this.value.pop()
-        if(this.value.length > 0){
-            this.value[0] = end
-            this.sinking()
-        }
-        return min
-    }
-    sinking(){
-        let inx = 0
-        let element = this.value[0]
-        let length = this.value.length
-        while(true){
-            let leftChildInx = inx * 2 + 1
-            let rightChildInx = inx * 2 + 2
-            let leftChild, rightChild
-            let swap = null
-            if(leftChildInx < length){
-                leftChild = this.value[leftChildInx]
-                if(leftChild.priority < element.priority){
-                    swap = leftChildInx
-                }
-            }
-            if(rightChildInx < length){
-                rightChild = this.value[rightChildInx]
-                if(rightChild.priority < element.priority && rightChild.priority < leftChild.priority){
-                    swap = rightChildInx
-                }
-            }
-            if(swap === null) break
-            this.value[inx] = this.value[swap]
-            this.value[swap] = element
-            inx = swap
-        }
-    }
-}
 
-let heap = new MaxBinaryHeap()
-
-heap.Insert("head ach", 3)
-heap.Insert("urgent", 1)
-heap.Insert("feaver", 2)
-heap.Insert("slite pain", 5)
-
-heap.extract()
-
-console.log(heap)
+let str = new HashTables()
+str.set("ra", "rajkrishna")
+str.set("ram", "eky")
+console.log(str)
